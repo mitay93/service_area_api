b9c0314454373e362203ef9adfbecbc4738b4210
Artem <artem@doclub.tv>
2024-05-30 19:59:53 +0200

    Redis URL for healthcheck

--- service_area_api/settings-production.py
+++ service_area_api/settings-production.py
@@ -103,6 +103,8 @@ CACHES = {
     }
 }
 
+REDIS_URL = f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}"
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
