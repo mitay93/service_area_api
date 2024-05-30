54f776dcbdd2950caff7fcc3d4f3f620f6ad7fb9
Artem <artem@doclub.tv>
2024-05-30 16:28:33 +0200

    Final config

--- service_area_api/settings-production.py
+++ service_area_api/settings-production.py
@@ -162,3 +162,13 @@ SPECTACULAR_SETTINGS = {
 
 GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
 GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
+
+AWS_S3_ACCESS_KEY_ID = env("AWS_S3_ACCESS_KEY_ID", "")
+AWS_S3_SECRET_ACCESS_KEY = env("AWS_S3_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", "")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", "")
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+}
